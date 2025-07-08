899a3c928b8427fc16d5ecfdb759efcbff6f8755
ebwb <ebwbernard@gmail.com>
2025-07-07 23:27:32 -0500

    Add file reading and parsing logic, tests, logs

--- src/record_parsing/core.clj
+++ src/record_parsing/core.clj
@@ -1,8 +1,17 @@
-(ns record-parsing.core)
+(ns record-parsing.core
+  (:require [clojure.string :as s]
+            [clojure.java.io :as io]
+            [clojure.tools.logging :as log]))
+
+;; TODO(ebwb): namespace is getting unwieldy; may be ok for
+;; command-line app, but will certainly need refactor before REST app
+;; is complete
 
 (def delimiters
   "Supported delimiters."
-  {\| :pipe \, :comma \space :space})
+  {:pipe \|
+   :comma \,
+   :space \space})
 
 (defn detect-delimiter
   "Identify the delimiter based on the contents of the given
@@ -12,7 +21,7 @@
   ;; TODO(ebwb): currently, this scans every character in the given
   ;; line for each known delimiter. we can likely scan the line once
   ;; and arrive at the same answer.
-  (let [counts (into {} (for [[char key] delimiters]
+  (let [counts (into {} (for [[key char] delimiters]
                           [key (count (filter #(= % char) line))]))
         sorted (sort-by val > counts)]
 
@@ -21,3 +30,52 @@
     (if (= (second (vals sorted)) (val (first sorted)))
       (throw (ex-info "Ambiguous delimiter detected" {:counts counts}))
       (key (first sorted)))))
+
+(defn valid-line?
+  [line]
+  (and (not (s/blank? line))
+       (not (s/starts-with? line "#"))))
+
+(defn ->record
+  "Parse data into a record."
+  [data]
+  {:last-name (nth data 0)
+   :first-name (nth data 1)
+   :email (nth data 2)
+   :favorite-color (nth data 3)
+   :dob (nth data 4)})
+
+(defn parse
+  "Parse a line of data into a record. Assumes that pattern has been
+  correctly identified for the given data."
+  [pattern data]
+  (-> data
+      (s/split pattern)
+      ->record))
+
+;; TODO(ebwb): could use a better name
+(defn process
+  "Process input file's valid lines into output."
+  [lines]
+  ;; drop noise lines
+  (let [data (filter valid-line? lines)
+        delimiter (detect-delimiter (first data))
+        split-pattern (->> delimiter
+                           (get delimiters)
+                           (str "\\")
+                           (re-pattern))]
+    (log/info "Delimiter used is" (name delimiter))
+
+    ;; parse and display data
+    (map (partial parse split-pattern) data)))
+
+(defn -main
+  [filepath]
+
+  ;; wrap around doall so file is processed before we close the
+  ;; stream, but still allow for a lazy seq to be passed around in
+  ;; case of a larger file.
+  (with-open [r (io/reader filepath)]
+    ;; TODO(ebwb): error handling, edge cases
+    (println 
+    (doall (process (line-seq r))))))
--- test/record_parsing/core_test.clj
+++ test/record_parsing/core_test.clj
@@ -1,6 +1,8 @@
 (ns record-parsing.core-test
   (:require [clojure.test :refer :all]
-            [record-parsing.core :as sut])
+            [record-parsing.core :as sut]
+            [clojure.java.io :as io]
+            [clojure.string :as s])
   (:import [clojure.lang ExceptionInfo]))
 
 
@@ -18,5 +20,56 @@
       (catch Exception e
         (is (= "Ambiguous delimiter detected" (.getMessage e)))
         (is (= {:counts {:pipe 0 :comma 0 :space 0}} (ex-data e)))))))))
+
+(deftest test-valid-line?
+  (testing "valid-line?"
+    (is (true? (sut/valid-line? "Foo")))
+    (is (true? (sut/valid-line? "123")))
+    (is (true? (sut/valid-line? "$%^")))
+    (is (false? (sut/valid-line? " ")))
+    (is (false? (sut/valid-line? "")))
+    (is (false? (sut/valid-line? nil)))
+    (is (false? (sut/valid-line? "#")))
+    (is (false? (sut/valid-line? "# ")))))
+    
+(deftest test-parse
+  (testing "parse parses valid records"
+    (is (= {:last-name "last"
+            :first-name "first"
+            :email "foo@bar.com"
+            :favorite-color "green"
+            :dob "1/5/2025"}
+           (sut/parse
+            #"\#"
+            "last#first#foo@bar.com#green#1/5/2025"))))
+  (testing "fails to parse lines that use wrong pattern"
+    (try
+      (sut/parse #"\," "last|first|foo@bar.com|green|1/5/2025")
+      (catch Exception e
+        (is java.lang.IndexOutOfBoundsException (type e)))))
+  (testing "fails to parse lines that don't have enough fields"
+    (try
+      (sut/parse #"\," "last,first,foo@bar.com,green")
+      (catch Exception e
+        (is java.lang.IndexOutOfBoundsException (type e))))))
         
-           
+
+(def comma-separated (-> "comma-delimited.txt"
+                         io/resource
+                         slurp
+                         s/split-lines))
+
+(deftest test-parse
+  (testing "full parse test"
+    (let [result (sut/process comma-separated)]
+      (is (= [{:last-name "Tirekicker"
+	      :first-name "Ruth"
+	      :email "ruth.tirekicker@yopmail.com"
+	      :favorite-color "black"
+	      :dob "2/7/1984"}
+	     {:last-name "Homeowner"
+	      :first-name "John"
+	      :email "john.homeowner@yopmail.com"
+	      :favorite-color "white"
+	      :dob "1/1/1980"}]
+             result)))))
